22b7cf95eefcafacc0c111495612afb308e0453b
foxy-1423 <krystofholan@gmail.com>
2023-10-16 11:59:15 +0200

    im gretting angy

--- test7.py
+++ test7.py
@@ -1,21 +1,12 @@
 import serial
 
-ser = serial.Serial('COM3', 9600)  # Replace 'COMx' with your actual serial port name
+ser = serial.Serial('COM3', 9600, timeout=1)
 
 while True:
-    line = ser.readline().decode('utf-8').strip()
-    print(f"Received: {line}")
-    
-    if line.startswith('R') and len(line) == 6:
-        try:
-            range_mm = int(line[1:5])
-            if range_mm == 5000:
-                print("No target detected")
-            else:
-                print(f"Range: {range_mm} mm")
-        except ValueError:
-            print("Invalid data format")
-    else:
-        print("Invalid data format")
-
-ser.close()
+    data = ser.readline().decode('ascii')
+    if data.startswith('R'):
+        range_in_mm = int(data[1:5])
+        if range_in_mm == 5000:
+            print('No target detected')
+        else:
+            print(f'Target detected at {range_in_mm} mm')
